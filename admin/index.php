8d2d4bd0e41b5798457484ef49b9f2af1f5bccd0
iMode79 <ricardo.labrada@me.com>
2024-04-03 18:20:17 -0600

    FinCdelCRUD

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,11 @@
 <?php 
+ini_set('display_errors', 1);
+    /*echo"<pre>";
+    var_dump($_GET);
+    echo"</pre>";
+    exit;*/
+    $resultado = $_GET['resultado'] ?? null;//Solo agarramos del url el valor de resultado
+
     require '../includesphp/funciones.php';
     incluirTemplate('header');
 ?> 
@@ -6,11 +13,13 @@
     <main class="contenedor">
         <h1>Administrador de Bienes Raices</h1>
 
+        <?php if (intval( $resultado === '1' )): ?>
+            <p class="alerta exito">Anuncio creado correctamente</p>
+            <?php endif; ?>
+
         <a href="/bienesraices_inicio/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
 
     </main>
-
-
 
 <?php 
     incluirTemplate('footer'); 
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -21,18 +21,29 @@
 
     //Ejecuta código después de que el usuario envia el formulario
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
         /*echo"<pre>";
         var_dump($_POST);
-        echo"</pre>";*/
+        echo"</pre>";
 
-        $titulo = $_POST['titulo'];
-        $precio = $_POST['precio'];
-        $descripcion = $_POST['descripcion'];
-        $habitaciones = $_POST['habitaciones'];
-        $wc = $_POST['wc'];
-        $estacionamiento = $_POST['estacionamiento'];
-        $vendedorId = $_POST['vendedor'];        
+        echo"<pre>";
+        var_dump($_FILES);
+        echo"</pre>";
+        exit;*/
 
+        $titulo = mysqli_real_escape_string($db, filter_var($_POST['titulo'],FILTER_SANITIZE_STRING));
+        $precio = mysqli_real_escape_string($db,  filter_var($_POST['precio'],FILTER_SANITIZE_NUMBER_FLOAT));  //$_POST['precio']);
+        $descripcion = mysqli_real_escape_string($db, filter_var($_POST['descripcion'],FILTER_SANITIZE_STRING));  //$_POST['descripcion']);
+        $habitaciones = mysqli_real_escape_string($db, filter_var($_POST['habitaciones'],FILTER_SANITIZE_NUMBER_FLOAT));  //$_POST['habitaciones']);
+        $wc = mysqli_real_escape_string($db, filter_var($_POST['wc'],FILTER_SANITIZE_NUMBER_FLOAT));  //$_POST['wc']);
+        $estacionamiento = mysqli_real_escape_string($db, filter_var($_POST['estacionamiento'],FILTER_SANITIZE_NUMBER_FLOAT));  //$_POST['estacionamiento']);
+        $vendedorId = mysqli_real_escape_string($db, filter_var($_POST['vendedor'],FILTER_SANITIZE_NUMBER_FLOAT));  //$_POST['vendedor']);  
+        $creado = date('Y/m/d');
+
+        //Validar carga de imagenes
+        $imagen = $_FILES['imagen'];
+        
+        //Validación de errores
         if (!$titulo) {
             $errores [] = "Debes añadir un titulo";
         }
@@ -54,6 +65,15 @@
         if (!$vendedorId) {
             $errores [] = "Debes añadir un vendedor";
         }
+
+        if (!$imagen['name'] || $imagen['error']) {
+            $errores [] = "La imagen es obligatoria";
+        }
+        //Validacion de carga de imagenes por tamaño (max 1mb)
+        $medida = 1000 * 1000;
+        if ($imagen['size'] > $medida) {
+            $errores [] = "La imagen es muy pesada";
+        }
         /*echo"<pre>";
         var_dump($errores);
         echo"</pre>";*/
@@ -61,14 +81,27 @@
         //Revisar que el arreglo este vacio
         if (empty($errores)) {
 
-            $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId')";
+            //Subida de archivos Si $errores esta vacio entonces sube archivo
+            //Creación de carpetas con ruta relativa
+            $carpetaImagenes = '../../imagenes/';
+            if (!is_dir($carpetaImagenes)) {
+                mkdir($carpetaImagenes);
+            }
+
+            //Generar nombre único
+            $nombreImagen = md5(uniqid(rand(),true)) . ".jpg";
+
+            //Subir imagen
+            move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen);            
+
+            //query de insercion de datos del formulario a la base de datos 
+            $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, wc, estacionamiento, creado, vendedorId) VALUES ('$titulo', '$precio', '$nombreImagen', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedorId')";
             //echo $query;
             //echo $creado;
-
             $resultado = mysqli_query($db, $query);
             if($resultado) {
                 //echo "Insertardo Correctamente";
-                header('Location: /bienesraices_inicio/admin');
+                header('Location: /bienesraices_inicio/admin?resultado=1');
             } 
         }
     }    
@@ -79,14 +112,14 @@
     <main class="contenedor">
         <h1>Crear</h1>
         <a href="/bienesraices_inicio/admin" class="boton boton-verde">Regresar</a>
-
+        
         <?php foreach($errores as $error): ?>
             <div class="alerta error">
             <?php echo $error; ?>
             </div>
         <?php endforeach; ?>
 
-        <form class="formulario" method="POST" action="/bienesraices_inicio/admin/propiedades/crear.php">
+        <form class="formulario" method="POST" action="/bienesraices_inicio/admin/propiedades/crear.php" enctype="multipart/form-data">
             <fieldset>
                 <legend>Información General de Nuestras Propiedades</legend>
 
